69cf059a12142622a7e2e9f5516460fbdceb1b0b
adatationio <willie@adaptation.io>
2018-11-12 17:00:01 +1030

    refactor: added multi processor baseline

---
+++ traderrl/baselinemulti.py
@@ -0,0 +1,82 @@
+import time
+
+import gym
+import numpy as np
+
+from stable_baselines.common.policies import MlpPolicy, MlpLnLstmPolicy
+from stable_baselines.common.vec_env import DummyVecEnv, SubprocVecEnv
+from stable_baselines.common import set_global_seeds
+from stable_baselines import ACKTR, PPO2
+from template_env import Template
+env = Template()
+
+def make_env(env_id, rank, seed=0):
+    """
+    Utility function for multiprocessed env.
+    
+    :param env_id: (str) the environment ID
+    :param num_env: (int) the number of environment you wish to have in subprocesses
+    :param seed: (int) the inital seed for RNG
+    :param rank: (int) index of the subprocess
+    """
+    def _init():
+        env = Template()
+        env.seed(seed + rank)
+        return env
+    set_global_seeds(seed)
+    return _init
+
+env_id = "CartPole-v1"
+num_cpu = 4  # Number of processes to use
+# Create the vectorized environment
+env = SubprocVecEnv([make_env(env_id, i) for i in range(num_cpu)])
+
+model = PPO2(MlpLnLstmPolicy, env, verbose=0)
+
+def evaluate(model, num_steps=1000):
+    """
+    Evaluate a RL agent
+    :param model: (BaseRLModel object) the RL Agent
+    :param num_steps: (int) number of timesteps to evaluate it
+    :return: (float) Mean reward
+    """
+    episode_rewards = [[0.0] for _ in range(env.num_envs)]
+    obs = env.reset()
+    for i in range(num_steps):
+      # _states are only useful when using LSTM policies
+      actions, _states = model.predict(obs)
+      # here, action, rewards and dones are arrays
+      # because we are using vectorized env
+      obs, rewards, dones, info = env.step(actions)
+      
+      # Stats
+      for i in range(env.num_envs):
+          episode_rewards[i][-1] += rewards[i]
+          if dones[i]:
+              episode_rewards[i].append(0.0)
+
+    mean_rewards =  [0.0 for _ in range(env.num_envs)]
+    n_episodes = 0
+    for i in range(env.num_envs):
+        mean_rewards[i] = np.mean(episode_rewards[i])     
+        n_episodes += len(episode_rewards[i])   
+
+    # Compute mean reward
+    mean_reward = round(np.mean(mean_rewards), 1)
+    print("Mean reward:", mean_reward, "Num episodes:", n_episodes)
+
+    return mean_reward
+
+
+mean_reward_before_train = evaluate(model, num_steps=10000)
+
+n_timesteps = 10000000
+
+# Multiprocessed RL Training
+start_time = time.time()
+model.learn(n_timesteps)
+total_time_multi = time.time() - start_time
+
+print("Took {:.2f}s for multiprocessed version - {:.2f} FPS".format(total_time_multi, n_timesteps / total_time_multi))
+
+mean_reward_before_train = evaluate(model, num_steps=10000)
